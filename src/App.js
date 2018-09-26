873352f024ae21cabfe91c89351aac53a95ff40e
Nitin Patel <nitinpatel278@gmail.com>
2018-09-27 00:09:35 +0530

    Completed API Integration & Search Functionality
    Completed API Integration and Search Functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,106 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import AllResults from "./components/AllResults";
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
+  state = {
+    searchText: "",
+    error: "",
+    fetchingData: false,
+    searchResults: []
+  };
+
+  onTextChange = e => {
+    this.setState({
+      searchText: e.target.value
+    });
+  };
+
+  onButtonClick = () => {
+    this.setState({
+      fetchingData: true
+    });
+    const { searchText } = this.state;
+    const requestUri =
+      `https://cors-anywhere.herokuapp.com/` +
+      `https://www.goodreads.com/search/index.xml?key=${apiKey}&q=${searchText}`;
+
+    Axios.get(requestUri)
+      .then(res => {
+        this.parseXMLResponse(res.data);
+      })
+      .catch(error => {
+        this.setState({
+          error: error.toString(),
+          fetchingData: false
+        });
+      });
+  };
+
+  // parse xml received from goodreads api
+  parseXMLResponse = response => {
+    const parser = new DOMParser();
+    const XMLResponse = parser.parseFromString(response, "application/xml");
+    const parseError = XMLResponse.getElementsByTagName("parsererror");
+
+    if (parseError.length) {
+      this.setState({
+        error: "There was an error fetching results.",
+        fetchingData: false
+      });
+    } else {
+      const XMLresults = new Array(...XMLResponse.getElementsByTagName("work"));
+      const searchResults = XMLresults.map(result => this.XMLToJson(result));
+      this.setState({ searchResults, fetchingData: false });
+    }
+  };
+
+  // Function to convert simple XML document into JSON.
+  XMLToJson = XML => {
+    const allNodes = new Array(...XML.children);
+    const jsonResult = {};
+    allNodes.forEach(node => {
+      if (node.children.length) {
+        jsonResult[node.nodeName] = this.XMLToJson(node);
+      } else {
+        jsonResult[node.nodeName] = node.innerHTML;
+      }
+    });
+    return jsonResult;
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="container mt-5">
+        <div className="form-group row">
+          <input
+            className="mr-1 col-sm-9 form-control"
+            type="text"
+            placeholder="Search Books By title, author, or ISBN..."
+            name="searchText"
+            onChange={this.onTextChange}
+            value={this.state.searchText}
+          />
+          <button
+            className="col-sm-2 btn btn-primary"
+            onClick={this.onButtonClick}
+          >
+            Search
+          </button>
+        </div>
+
+        {/**
+         * if fetching data, display "loading...", if error, display error message, else display search results
+         */}
+        {this.state.fetchingData ? (
+          <p className="lead text-center">{"loading... "}</p>
+        ) : (
+          (this.state.error && (
+            <p className="text-danger">{this.state.error}</p>
+          )) || <AllResults books={this.state.searchResults} />
+        )}
       </div>
     );
   }
